354e3291d109976f158b6a6d8313f039c738b965
ivanmaryanin <imaryanin@gmail.com>
2022-09-23 03:24:16 +0700

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,60 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var ErrInvalidWordForRegexp = errors.New("panic word for regexp")
+
+func getRegexpWord(w string, re *regexp.Regexp) string {
+	if word := re.ReplaceAllString(w, ""); word != "" {
+		return strings.ToLower(word)
+	}
+	return ""
+}
+
+func Top10(s string) []string {
+	defer func() {
+		if p := recover(); p != nil {
+			fmt.Println(ErrInvalidWordForRegexp)
+			return
+		}
+	}()
+
+	const limitTop = 10
+	counterWords := make(map[string]int)
+	var uniqWords []string
+
+	re := regexp.MustCompile(`(^'|'$|'[?=,]|[?<=,]')|([,!.]$)|^-$`)
+
+	for _, w := range strings.Fields(s) {
+		word := getRegexpWord(w, re)
+		if word == "" {
+			continue
+		}
+
+		if _, ok := counterWords[word]; !ok {
+			uniqWords = append(uniqWords, word)
+		}
+		counterWords[word]++
+	}
+
+	sort.Slice(uniqWords, func(i, j int) bool {
+		firstWord := uniqWords[i]
+		secWord := uniqWords[j]
+		if counterWords[firstWord] == counterWords[secWord] {
+			return firstWord < secWord
+		}
+		return counterWords[firstWord] > counterWords[secWord]
+	})
+
+	if len(uniqWords) > limitTop {
+		return uniqWords[:limitTop]
+	}
+
+	return uniqWords
 }
